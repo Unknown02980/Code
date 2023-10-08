ae89b072aebc379a4f333811512c22207ef3f314
Unknown02980 <147297499+Unknown02980@users.noreply.github.com>
2023-10-08 19:34:29 +0530

    1stcode

---
+++ COMPLEXCLASS.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+class Complex{  
+    int real;
+    int imaginary;
+    public:
+    Complex()
+    {
+        this->real=0;
+        this->imaginary=0;
+    }
+    Complex(int r,int i)
+    {
+        this->real=r;
+        this->imaginary=i;
+    }
+    void calculation()
+    {
+        cout<<add()<<endl<<subtract<<endl<<multiply<<endl<<divide<<endl;
+    }
+    private:
+    Complex add(Complex c2)
+    {
+        
+    }
+    Complex subtract(Complex c2)
+    {
+
+    }
+    Complex multiply()
+    {
+
+    }
+    Complex divide()
+    {
+
+    }
+}c1,c2,c3,c4;
+int main()
+{
+
+}
